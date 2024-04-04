dff2b1315f770cc16fbb33cdfde7df1d72cc9ad5
alexdevzen <alex.devzen@gmail.com>
2024-04-03 23:18:24 -0300

    Skills component

---
+++ src/utils/data.js
@@ -0,0 +1,39 @@
+export const SKILLS = [
+  {
+    title: "Frontend",
+    icon: "./assets/images/frontend-icon.png",
+    skills: [
+      { skills: "HTML5", percentage: "80%" },
+      { skills: "CSS3 (Sass/SCSS", percentage: "90%" },
+      { skills: "JavaScript", percentage: "75%" },
+      { skills: "React.js", percentage: "80%" },
+    ],
+  },
+  {
+    title: "Backend",
+    icon: "./assets/images/backend-icon.png",
+    skills: [
+      { skills: "Node.js", percentage: "70%" },
+      { skills: "Express.js", percentage: "65%" },
+    ],
+  },
+  {
+    title: "Tools",
+    icon: "./assets/images/tools-icon.png",
+    skills: [
+      { skills: "Git & GitHub", percentage: "85%" },
+      { skills: "Visual Studio Code", percentage: "75%" },
+      { skills: "Webpack", percentage: "60%" },
+      { skills: "Responsive Design", percentage: "70%" },
+    ],
+  },
+  {
+    title: "Soft Skills",
+    icon: "./assets/images/soft-skills-icon.png",
+    skills: [
+      { skills: "Problem-solving", percentage: "80%" },
+      { skills: "Collaboration", percentage: "85%" },
+      { skills: "Attention to Detail", percentage: "75%" },
+    ],
+  },
+];
