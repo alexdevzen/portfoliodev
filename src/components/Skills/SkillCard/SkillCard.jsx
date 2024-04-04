11204cadd9bf1f6aa0421cad02e8fb7fb8b60798
alexdevzen <alex.devzen@gmail.com>
2024-04-04 13:43:14 -0300

    Style skill card

--- src/components/Skills/SkillCard/SkillCard.jsx
+++ src/components/Skills/SkillCard/SkillCard.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import "./SkillCard.css";
 
 const SkillCard = ({ title, iconUrl, isActive, onClick }) => {
+  
   return (
     <div className={`skills-card ${isActive ? "active" : ""}`} onClick={() => onClick()}>
+
         <div className="skill-icon">
             <img src={iconUrl} alt={title} />
         </div>
